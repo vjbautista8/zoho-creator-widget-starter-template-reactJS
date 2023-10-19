414b3bb0b9c562e66e3404ebac0e57619fa87e92
vjbautista8 <vjbautista8@gmail.com>
2023-10-19 23:13:47 +0800

    add npm run zoho-creator for local development

--- react_app/src/index.js
+++ react_app/src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+window.ZOHO.CREATOR.init().then(function () {
+  let loginUserInfo = window.ZOHO.CREATOR.UTIL.getInitParams();
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+    <React.StrictMode>
+      <App loginUserInfo={loginUserInfo} />
+    </React.StrictMode>
+  );
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
